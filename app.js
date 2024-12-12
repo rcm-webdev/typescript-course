cf8255df22d4318cb2b88b419af26aaf832b74f9
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-12-12 15:24:20 -0800

    create a utility function that takes a pizza object and adds it to the menu

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const menu = [
   { name: Pepperoni, price: 3.0 },
   { name: Veggie, price: 3.5 },
 ];
+
+const cashInRegister = 100;
+const orderQueue = [];
+
+/**
+ * Challenge: Add a utility function "addNewPizza" that takes a pizza object
+ * and adds it to the menu.
+ */
+
+const addNewPizza = (pizzaObj) => {
+  menu.push(pizzaObj);
+};
